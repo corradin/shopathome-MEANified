a674a00899e6d7c97719491499c83476b0d24df7
Riccardo Corradin <riccardo.corradin@live.com>
2020-11-20 15:37:23 +0100

    Added mock data

--- angular-app/api/server.ts
+++ angular-app/api/server.ts
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 7071;
 
+const productsMock = [{
+  id: 1,
+  name: 'Xbox Series X',
+  description: 'The latest gen console',
+  quantity: 1
+}];
+
 app.get('/api/products', (req, res) => {
-  res.send('Hello World!');
+  res.send(productsMock);
 });
 
 app.listen(port, () => {
